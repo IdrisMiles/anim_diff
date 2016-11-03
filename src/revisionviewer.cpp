0f5e4218a434fb8fbf7b969f7301e384cd4c44be
Idris Miles <idris.miles@gmail.com>
2016-11-03 14:10:40 +0000

    fixed glm error on uni machine

--- src/revisionviewer.cpp
+++ src/revisionviewer.cpp
@@ -47,7 +47,7 @@ void RevisionViewer::paintGL()
 
     glUniformMatrix4fv(m_projMatrixLoc, 1, false, &m_projMat[0][0]);
     glUniformMatrix4fv(m_mvMatrixLoc, 1, false, &(m_modelMat*m_viewMat)[0][0]);
-    glm::mat3 normalMatrix =  glm::inverse(m_modelMat);
+    glm::mat3 normalMatrix =  glm::inverse(glm::mat3(m_modelMat));
     glUniformMatrix3fv(m_normalMatrixLoc, 1, true, &normalMatrix[0][0]);
 
 
@@ -68,7 +68,7 @@ void RevisionViewer::LoadRevision(std::shared_ptr<RevisionNode> _revision)
 
     const aiScene *scene = _revision->m_model->m_scene;
 
-    std:cout << scene << "\n";
+    std::cout << scene << "\n";
 
     if(!scene)
     {
