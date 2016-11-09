32a89c16748c3069d062bfe1f095349cb3e191aa
gmadges <gmadgesdeveloper@gmail.com>
2016-11-09 15:11:54 +0000

    added some basic stuff

--- include/revisionUtils.h
+++ include/revisionUtils.h
@@ -1,11 +1,19 @@
 #ifndef REVISIONUTILS_H
 #define REVISIONUTILS_H
 
+#include <memory>
+
+class RevisionNode;
+
 class RevisionUtils
 {
 public:
     RevisionUtils();
     ~RevisionUtils();
+
+    // will not be void once structure is created
+    static void getDiff(std::shared_ptr<RevisionNode> _master, std::shared_ptr<RevisionNode> _branch);
+
 };
 
 #endif // REVISIONUTILS_H
--- src/revisionUtils.cpp
+++ src/revisionUtils.cpp
@@ -1,5 +1,8 @@
 #include "revisionUtils.h"
 
+#include "revisionNode.h"
+#include <assimp/scene.h>
+
 RevisionUtils::RevisionUtils()
 {
 
@@ -7,5 +10,12 @@ RevisionUtils::RevisionUtils()
 
 RevisionUtils::~RevisionUtils()
 {
-    
+
+}
+
+void RevisionUtils::getDiff(std::shared_ptr<RevisionNode> _master, std::shared_ptr<RevisionNode> _branch)
+{   
+    // public members, so so bad and dirty raw pointers
+    const aiScene *masterScene = _master->m_model->m_scene;
+    const aiScene *branchScene = _branch->m_model->m_scene;
 }
