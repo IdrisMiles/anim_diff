d22e443af4b4e2eaa5795c681bcf32bee236f38d
Idris Miles <idris.miles@gmail.com>
2016-12-01 15:51:43 +0000

    removing mergedRevision shiz

---
+++ include/diffViewer.h
@@ -0,0 +1,11 @@
+#ifndef DIFFVIEWER_H
+#define DIFFVIEWER_H
+
+
+class DiffViewer
+{
+public:
+    DiffViewer();
+};
+
+#endif // DIFFVIEWER_H
---
+++ src/diffViewer.cpp
@@ -0,0 +1,6 @@
+#include "include/diffViewer.h"
+
+DiffViewer::DiffViewer()
+{
+
+}
